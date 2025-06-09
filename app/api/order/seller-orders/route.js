96cda7ff86ee50fbcc7275fca41519b0399c8e40
NinoW <nino.wandhana@gmail.com>
2025-06-09 22:55:44 +0700

    docker

--- app/api/order/seller-orders/route.js
+++ app/api/order/seller-orders/route.js
@@ -1,4 +1,4 @@
-import { getAuth } from "@/lib/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import authSeller from "@/lib/authSeller";
 import { connect } from "mongoose";
