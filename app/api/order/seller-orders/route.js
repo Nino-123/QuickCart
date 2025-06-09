df6e539e71cceda41fb1a2a6781dda5dea6b6001
NinoW <nino.wandhana@gmail.com>
2025-06-09 22:21:22 +0700

    run

---
+++ app/api/order/seller-orders/route.js
@@ -0,0 +1,32 @@
+import { getAuth } from "@/lib/nextjs/server";
+import { NextResponse } from "next/server";
+import authSeller from "@/lib/authSeller";
+import { connect } from "mongoose";
+import Address from "@/models/Address";
+
+
+
+export async function GET(request) {
+    try {
+        
+        const { userId } = getAuth(request);
+
+        const isSeller = await authSeller(userId);
+
+        if (!isSeller) {
+            return NextResponse.json({ success: false, message: "Unauthorized" })
+        }
+
+        await connectDB()
+
+        Address.length
+
+        const addresses = await Order.find({}).populate('address items.product')
+
+        return NextResponse.json({ success: true, orders });
+
+    } catch (error) {
+        return NextResponse.json({ success: false, message: 'Authorized' })
+    }
+
+}
